d5684951cca91b0713d523be65ff343f3a060bc7
Alan Saji <rockstaralansaji@gmail.com>
2024-08-01 12:04:53 +0530

    update readme

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,9 +1,8 @@
 /** @type { import("drizzle-kit").Config } */
 export default {
-    schema: "./utils/schema.jsx",
-    dialect: 'postgresql',
-    dbCredentials: {
-      url: "postgresql://neondb_owner:8l1pCxksgLGQ@ep-shrill-cloud-a19zt97v.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
-    }
-  };
-  
+  schema: "./utils/schema.jsx",
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.NEXT_PUBLIC_DATABASE_URL,
+  }
+};
--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -1,7 +1,8 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
-const sql = neon("postgresql://neondb_owner:8l1pCxksgLGQ@ep-shrill-cloud-a19zt97v.ap-southeast-1.aws.neon.tech/neondb?sslmode=require");
-export const db = drizzle(sql, {schema});
+
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
+export const db = drizzle(sql, { schema });
 
 
